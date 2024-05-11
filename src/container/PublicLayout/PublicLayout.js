15abf0181a3e3a779964714fd2d525a2ef6705ca
Suresh <83566549+Suresh-48@users.noreply.github.com>
2024-05-11 15:48:44 +0530

    Update PublicLayout.js

--- src/container/PublicLayout/PublicLayout.js
+++ src/container/PublicLayout/PublicLayout.js
@@ -1,38 +1,35 @@
-import React, { Component, Suspense, useState } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import { Switch, Route } from "react-router-dom";
 import SideBar from "../../components/SideBar";
 import HeaderNavBar from "../../components/HeaderNavBar";
-
-// routes config
 import routes from "../../routes";
-
-// Styles
 import "../../css/SideBar.scss";
+
 const DefaultLayout = (props) => {
-  // const propsValue = props?.location?.props?.data ? props?.location?.props?.data : props?.location?.state?.previousData;
   const openSideBar = props?.location?.state?.state;
-  const productId = props?.location?.state?.productId
-    ? props?.location?.state?.productId
-    : props?.location?.props?.data?.id;
-  const projectId = props?.location?.state?.projectId
-    ? props?.location?.state?.projectId
-    : props?.computedMatch?.params?.id;
+  const productId = props?.location?.state?.productId || props?.location?.props?.data?.id;
+  const projectId = props?.location?.state?.projectId || props?.computedMatch?.params?.id;
 
   const [active, setActive] = useState(false);
 
-  const sessionId = localStorage.getItem("sessionId");
-  const loading = () => <div className="animated fadeIn pt-1 text-center">Loading...</div>;
-  const value = () => {
-    setActive(!active);
+  useEffect(() => {
+    const savedSideBarValue = localStorage.getItem("sideBarValue");
+    setActive(savedSideBarValue === "true");
+  }, []); 
+
+  const toggleActive = () => {
+    const updatedValue = !active;
+    setActive(updatedValue);
+    localStorage.setItem("sideBarValue", updatedValue.toString());
   };
 
   return (
     <div className="app">
       <div className="app-body" style={{ minHeight: "calc(100vh - 123px)" }}>
         <div>
-          {sessionId ? (
+          {localStorage.getItem("sessionId") ? (
             <SideBar
-              onClick={value}
+              onClick={toggleActive}
               value={projectId}
               active={active}
               props={productId}
@@ -46,7 +43,7 @@ const DefaultLayout = (props) => {
             selectedComponent={props?.selectedComponent === "FTA" ? "FTA" : null}
           />
           <div>
-            {sessionId ? (
+            {localStorage.getItem("sessionId") ? (
               <div className={`${active ? "site-maincontent home-content" : "site-maincontent active home-content"}`}>
                 <Suspense>
                   <Switch>
@@ -60,7 +57,7 @@ const DefaultLayout = (props) => {
                           render={(props) => <route.component {...props} />}
                         />
                       ) : (
-                        ""
+                        null
                       );
                     })}
                   </Switch>
@@ -80,7 +77,7 @@ const DefaultLayout = (props) => {
                           render={(props) => <route.component {...props} />}
                         />
                       ) : (
-                        ""
+                        null
                       );
                     })}
                   </Switch>
