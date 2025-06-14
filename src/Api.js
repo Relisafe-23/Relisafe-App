a1406f624c1134199ad7e6e2f874cd202bd2d553
Suresh-48 <83566549+Suresh-48@users.noreply.github.com>
2025-06-13 21:56:26 -0700

    base url changes

--- src/Api.js
+++ src/Api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-// const Api = axios.create({
-//   baseURL: "https://relisafe-api.herokuapp.com//",
-// });
 const Api = axios.create({
-  baseURL: "http://localhost:8000/",
+  baseURL: "https://relisafe-api.herokuapp.com//",
 });
+// const Api = axios.create({
+//   baseURL: "http://localhost:8000/",
+// });
 
 export default Api;
