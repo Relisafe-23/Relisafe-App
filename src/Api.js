0c97c73b42e593800a62733175516b285c344ccc
Suresh <seyoneprojects@gmail.com>
2025-09-26 14:56:33 +0530

    Update Api.js

--- src/Api.js
+++ src/Api.js
@@ -3,12 +3,12 @@ import axios from "axios";
 // const Api = axios.create({
 //   baseURL: "https://relisafe-api.herokuapp.com//",
 // });
-// const Api = axios.create({
-//   baseURL: "https://relisafe-api-e067e0a7c5f8.herokuapp.com/",
-// });
-
 const Api = axios.create({
-  baseURL: "http://localhost:8000/",
+  baseURL: "https://relisafe-api-e067e0a7c5f8.herokuapp.com/",
 });
+
+// const Api = axios.create({
+//   baseURL: "http://localhost:8000/",
+// });
 
 export default Api;
